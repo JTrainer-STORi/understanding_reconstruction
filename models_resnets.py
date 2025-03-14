b88a0d3a6c052544fb80dd23e02e87d42fd13b55
JTrainer-STORi <j.trainer1@lancaster.ac.uk>
2025-03-14 14:38:34 +0000

    fix2

--- models_resnets.py
+++ models_resnets.py
@@ -400,7 +400,7 @@ class ResNet(nn.Module):
                         kernel_init=self.kernel_init if self.param_dict is None else lambda *_ : jnp.array(self.param_dict['fc']['weight']), 
                         bias_init=self.bias_init if self.param_dict is None else lambda *_ : jnp.array(self.param_dict['fc']['bias']),
                         dtype=self.dtype)(x)
-            act['fc'] = x
+        act['fc'] = x
         
         if self.output == 'softmax':
             return nn.softmax(x)
